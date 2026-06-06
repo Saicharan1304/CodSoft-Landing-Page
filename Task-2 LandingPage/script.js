51f3b9911be8e838732d8ed883f554f142e15648
Saicharan1304 <msaicharan987@gmail.com>
2026-06-06 15:56:00 +0530

    Add files via upload

---
+++ Task-2 LandingPage/script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    alert("Welcome to CodeSpark! Start your learning journey today.");
+});
